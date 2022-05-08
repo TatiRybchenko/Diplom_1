c6b8afae187d565663e3cfc3642e2557d7c44c1f
Tati Rybchenko <ryb4enkota@gmail.com>
2022-05-08 19:00:24 +0300

    git test jacoco

--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -2,16 +2,33 @@ package praktikum;
 
 import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static praktikum.IngredientType.FILLING;
+import static praktikum.IngredientType.SAUCE;
 
+@RunWith(MockitoJUnitRunner.class)
 public class IngredientTypeTest extends TestCase {
 
+    @Mock
+    Ingredient  ingredient;
     @Test
-    public void values() {
+    public void valuesIngredientTypeSauce() {
+
+        Ingredient  ingredient = new Ingredient(SAUCE,"sour cream",200);
+        String expectedIngredientType = String.valueOf(SAUCE);
+        String actualIngredientType = String.valueOf(ingredient.getType());
+        assertEquals("Некорректное наименование типа ингредиента",expectedIngredientType,actualIngredientType);
     }
 
     @Test
-    public void valueOf() {
+    public void valueIngredientTypeFilling() {
+        Ingredient  ingredient = new Ingredient(FILLING,"sausage",300);
+        String expectedIngredientType = String.valueOf(FILLING);
+        String actualIngredientType = String.valueOf(ingredient.getType());
+        assertEquals("Некорректное наименование типа ингредиента",expectedIngredientType,actualIngredientType);
     }
 }
