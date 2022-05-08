d12c76a72771ce72f2b2a1910c6cc106eab1d8b9
Tati Rybchenko <ryb4enkota@gmail.com>
2022-05-08 20:09:02 +0300

    git burger test

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -6,8 +6,10 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
@@ -18,68 +20,52 @@ public class BurgerTest extends TestCase {
     private Ingredient ingredient;
     @Mock
     private Bun bun;
-    @Mock
-    Burger burgerMock = new Burger();
-    @Test
-    public void setBuns() {
-        Burger burger = new Burger();
-        burgerMock.setBuns(burgerMock.bun);
-        Mockito.verify(burgerMock).setBuns(burgerMock.bun);
-    }
 
     @Test
     public void addIngredient() {
         Burger burger = new Burger();
         burger.addIngredient(ingredient);
-        assertEquals(ingredient,burger.ingredients.get(0));
-    }
 
+        String expectedАddIngredient = String.valueOf(ingredient);
+        String actualАddIngredient = String.valueOf(burger.ingredients.get(0));
+        assertEquals("Добавление ингредиента не произошло", expectedАddIngredient, actualАddIngredient);
+    }
     @Test
-    public void removeIngredient() {
+    public void removeIngredient(){
         Burger burger = new Burger();
-        List<Ingredient> expected = new ArrayList<>();
         burger.addIngredient(ingredient);
         burger.removeIngredient(0);
-        assertEquals(expected,burger.ingredients);
+
+        List<Ingredient> expectedRemoveIngredient = new ArrayList<>();
+        List<Ingredient> actualRemoveIngredient = burger.ingredients;
+        assertEquals("Удаление ингредиента из списка не произошло", expectedRemoveIngredient, actualRemoveIngredient);
     }
 
     @Test
     public void moveIngredient() {
         Burger burger = new Burger();
-        burger.addIngredient(new Ingredient(SAUCE,"hot sauce",100));
-        burger.addIngredient(new Ingredient(FILLING,"sour cream",200));
-        burger.moveIngredient(0,1);
+        burger.addIngredient(new Ingredient(SAUCE,"sour cream",200));
+        burger.addIngredient(new Ingredient(FILLING,"dinosaur",200));
+        burger.addIngredient(new Ingredient(FILLING,"sausage",300));
+        burger.moveIngredient(0,2);
 
-        assertEquals("hot sauce",burger.ingredients.get(1).name);
+        String expectedMoveIngredient = "sausage";
+        String actualMoveIngredient = burger.ingredients.get(1).name;
+        assertEquals("Перемещение ингредиента не произошло", expectedMoveIngredient, actualMoveIngredient);
     }
 
     @Test
     public void getPrice() {
         Burger burger = new Burger();
-        float price = 12.99f;
+        float price = 500;
         Mockito.when(bun.getPrice()).thenReturn(price);
         Mockito.when(ingredient.getPrice()).thenReturn(price);
-
         burger.setBuns(bun);
         burger.addIngredient(ingredient);
 
-        assertEquals(38.97f,burger.getPrice(),0);
+        float expectedPriceBurger = 1500;
+        float actualPriceBurger = burger.getPrice();
+        assertEquals("Цена бургера некорректная", expectedPriceBurger, actualPriceBurger);
     }
 
-    @Test
-    public void getReceipt() {
-        Burger burger = new Burger();
-        burger.setBuns(bun);
-        burger.addIngredient(ingredient);
-
-        Mockito.when(bun.getName()).thenReturn("Булка");
-        Mockito.when(bun.getPrice()).thenReturn(12.99f);
-        Mockito.when(ingredient.getType()).thenReturn(SAUCE);
-        Mockito.when(ingredient.getName()).thenReturn("Кепчук");
-        Mockito.when(ingredient.getPrice()).thenReturn(12.99f);
-
-        String expected ="(==== Булка ====)" +"\r\n" + "= sauce Кепчук =" +"\r\n"+"(==== Булка ====)"+"\r\n"+"\r\n"+"Price: 38,970001"+"\r\n";
-
-        assertEquals(expected,burger.getReceipt());
     }
-}
--- src/test/java/praktikum/IngredientTypeTest.java
+++ src/test/java/praktikum/IngredientTypeTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.*;
 import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
