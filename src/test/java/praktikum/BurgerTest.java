af156b9f2342ee7c02a506c8268f6cf3dc41b07f
Tati Rybchenko <ryb4enkota@gmail.com>
2022-05-08 20:35:34 +0300

    git getReceipt

--- src/test/java/praktikum/BurgerTest.java
+++ src/test/java/praktikum/BurgerTest.java
@@ -6,10 +6,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import static praktikum.IngredientType.FILLING;
 import static praktikum.IngredientType.SAUCE;
 
@@ -26,9 +24,9 @@ public class BurgerTest extends TestCase {
         Burger burger = new Burger();
         burger.addIngredient(ingredient);
 
-        String expectedАddIngredient = String.valueOf(ingredient);
-        String actualАddIngredient = String.valueOf(burger.ingredients.get(0));
-        assertEquals("Добавление ингредиента не произошло", expectedАddIngredient, actualАddIngredient);
+        String expectedAddIngredient = String.valueOf(ingredient);
+        String actualAddIngredient = String.valueOf(burger.ingredients.get(0));
+        assertEquals("Добавление ингредиента не произошло", expectedAddIngredient, actualAddIngredient);
     }
     @Test
     public void removeIngredient(){
@@ -66,6 +64,21 @@ public class BurgerTest extends TestCase {
         float expectedPriceBurger = 1500;
         float actualPriceBurger = burger.getPrice();
         assertEquals("Цена бургера некорректная", expectedPriceBurger, actualPriceBurger);
+    }
+    @Test
+    public void getReceipt(){
+        Burger burger = new Burger();
+        burger.setBuns(bun);
+        burger.addIngredient(ingredient);
+        Mockito.when(bun.getName()).thenReturn("white bun");
+        Mockito.when(bun.getPrice()).thenReturn(200f);
+        Mockito.when(ingredient.getType()).thenReturn(SAUCE);
+        Mockito.when(ingredient.getName()).thenReturn("hot sauce");
+        Mockito.when(ingredient.getPrice()).thenReturn(100f);
+
+        String expectedGetReceipt = "(==== white bun ====)" +"\r\n" + "= sauce hot sauce =" +"\r\n"+"(==== white bun ====)"+"\r\n"+"\r\n"+"Price: 500,000000"+"\r\n";
+        String actualGetReceipt = burger.getReceipt();
+        assertEquals("Некорректный рецепт бургера", expectedGetReceipt, actualGetReceipt);
     }
 
     }
